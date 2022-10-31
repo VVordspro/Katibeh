f516b3c9a0e85f5a6703fc43a453e8292d3a8323
vvords <vvords@onionmail.com>
2022-10-31 13:05:19 +0330

    fee manager edited

--- contracts/utils/FeeManager.sol
+++ contracts/utils/FeeManager.sol
@@ -11,15 +11,16 @@ abstract contract FeeManager {
         address[] calldata receivers, 
         uint16[] calldata fractions
     ) internal {
-        _pay(creator, paidAmount * 70/100);
-        _pay(receiver1, paidAmount * 3/100);
+        _pay(creator, paidAmount * 750/1000);
+        _pay(receiver1, paidAmount * 25/1000);
 
         require(
             receivers.length == fractions.length,
             "FeeManager: receivers and fractions must be the same length"
         );
 
-        uint256 dappShare = paidAmount * 27/100;
+        uint256 dappShare = paidAmount - (paidAmount * 25/1000 + paidAmount * 750/1000);
+        
         uint16 totalFractions;
         for(uint8 i; i < fractions.length; i++) {
             totalFractions += fractions[i];
